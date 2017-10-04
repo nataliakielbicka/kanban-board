a99598ba42c3493d0ca78c1abe49f22c6e368514
Natalia <natalia.kielbicka@o2.pl>
2017-10-04 16:50:31 +0200

    create onClick event to open cardDetails

--- src/components/views/Card.jsx
+++ src/components/views/Card.jsx
@@ -2,14 +2,31 @@ import React, {Component} from "react";
 import CheckList from "./CheckList";
 
 export default class Card extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showDetails: false
+        }
+    }
     render() {
-        return (
-            <div className="card">
-                <div className="card__title">{this.props.title}</div>
+        const {showDetails} = this.state;
+        let cardDetails;
+        if (showDetails) {
+            cardDetails = (
                 <div className="card__details">
                     {this.props.description}
                     <CheckList key={this.props.id} cardId={this.props.id} tasks={this.props.tasks}/>
                 </div>
+            )
+        }
+        return (
+            <div className="card">
+                <div
+                    className="card__title"
+                    onClick={() => this.setState({
+                    showDetails: !showDetails
+                })}>{this.props.title}</div>
+                {cardDetails}
             </div>
         )
     }
